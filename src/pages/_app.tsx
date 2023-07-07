0c634622c0bd0df1b1aeeeaa75d5e660adea98cd
Alixujaev <alixujaevislom@gmail.com>
2023-07-08 00:49:01 +0500

    change script place

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,6 +26,18 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <MainContextProvider>
       <Layout>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy="afterInteractive" />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
         <Component {...pageProps} />
       </Layout>
       <ToastContainer />
