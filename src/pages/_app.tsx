0e445d09a6c7e0d2d6f8ecdb0bcca3386a816f1a
Alixujaev <alixujaevislom@gmail.com>
2023-07-20 01:18:57 +0500

    add scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,6 +26,27 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <MainContextProvider>
       <Layout>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T92378W7');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T92378W7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Script src={`https://www.googletagmanager.com/gtag/js?id=G-JWRJEZ358E`} strategy="afterInteractive" />
 
         <Script id="google-analytics" strategy="afterInteractive">
