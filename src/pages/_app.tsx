dbbba71e3592a8be9658a03fbf17dcdd35409ba2
Asadbek_back <asadbek_back@olcha.uz>
2023-10-21 15:39:06 +0500

    change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,7 +46,25 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-
+        <Script strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '873501004292423');
+          fbq('track', 'PageView');
+          `}
+        
+        </Script>
+        
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=873501004292423&ev=PageView&noscript=1"
+      /></noscript>
         <Script src={`https://www.googletagmanager.com/gtag/js?id=G-JWRJEZ358E`} strategy="afterInteractive" />
 
         <Script id="google-analytics" strategy="afterInteractive">
