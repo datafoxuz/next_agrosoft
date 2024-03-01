bcb8f3603d46415537724be9e2aeb7caae84f0f7
Asadbek Fayzulloev <fayzulloevasadbek@gmail.com>
2024-03-01 14:41:35 +0500

    fx

--- src/components/cardsCollection/components/communityCard/CommunityCard.tsx
+++ src/components/cardsCollection/components/communityCard/CommunityCard.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import React from "react";
 import CardActions from "./cardActions/CardActions";
 import Image from "next/image";
-
+import notfound from "@/assets/images/notfound.png"
 import styles from "./communitycard.module.scss";
 
 const CommunityCard = ({ data }: { data: card }) => {
@@ -15,7 +15,7 @@ const CommunityCard = ({ data }: { data: card }) => {
       onClick={() => router.push(`${router.pathname}/${data.slug}`)}
     >
       <Image
-        src={data.image ? data.image : ""}
+        src={data.image ? data.image : notfound}
         alt={`image about ${data.title}`}
         className={styles.image}
         width={220}
